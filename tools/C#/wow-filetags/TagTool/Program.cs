9ce11389cc0bfeb16ddbfffd4eb6b8a095973a6e
Martin Benjamins <ictmartin@gmail.com>
2026-01-04 19:01:22 +0100

    Add tag function to CLI test tool

--- tools/C#/wow-filetags/TagTool/Program.cs
+++ tools/C#/wow-filetags/TagTool/Program.cs
@@ -4,9 +4,12 @@
     {
         static void Main(string[] args)
         {
-            if (args.Length != 2)
+            if (args.Length < 2)
             {
-                Console.WriteLine("Usage: TagTool <mode> <path_to_repo>");
+                Console.WriteLine("Usage: TagTool <mode> <arguments>");
+                Console.WriteLine("Supported modes:");
+                Console.WriteLine("  rewrite <repository_path>");
+                Console.WriteLine("  tag <repository_path> <fdid> <tagKey> <tagValue> <tagSource>");
                 return;
             }
 
@@ -16,6 +19,20 @@
             {
                 var repo = new WoWTagLib.DataSources.Repository(args[1], verify: true, verbose: true);
                 repo.Save();
+            }
+            else if (mode == "tag")
+            {
+                var repo = new WoWTagLib.DataSources.Repository(args[1], verify: true, verbose: true);
+                int fdid = int.Parse(args[2]);
+                string tagKey = args[3];
+                string tagValue = args[4];
+                string tagSource = args[5];
+                repo.AddTagToFDID(fdid, tagKey, tagSource, tagValue);
+                repo.Save();
+            }
+            else
+            {
+                Console.WriteLine("Unknown mode. Supported modes are: rewrite, tag");
             }
         }
     }
